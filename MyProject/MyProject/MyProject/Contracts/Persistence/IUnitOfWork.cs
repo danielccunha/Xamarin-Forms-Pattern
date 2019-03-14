0c25caebfb4ccf26e95fc60f9193324d6a6c2abd
danielccunha <danielcunha54@gmail.com>
2019-03-14 18:11:48 -0300

    Initialized repository pattern

---
+++ MyProject/MyProject/MyProject/Contracts/Persistence/IUnitOfWork.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace MyProject.Contracts.Persistence
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        // TODO: Add repositories
+
+        int Complete();
+    }
+}
