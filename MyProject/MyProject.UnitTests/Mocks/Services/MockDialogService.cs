31ef81c5591648283312b5a45e3cc2f40ef5ef81
danielccunha <danielcunha54@gmail.com>
2019-03-18 09:39:31 -0300

    Add tests for MainViewModel

---
+++ MyProject/MyProject.UnitTests/Mocks/Services/MockDialogService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using Acr.UserDialogs;
+using MyProject.Contracts.Services.General;
+
+namespace MyProject.UnitTests.Mocks.Services
+{
+    public class MockDialogService : IDialogService
+    {
+        // TODO: Implement MockDialogService
+        public Task<string> DisplayActionSheetAsync(string title, string cancel, string destruction, params string[] buttons)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DisplayAlertAsync(string title, string message, string cancel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DisplayAlertAsync(string title, string message, string accept, string cancel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowToast(string message, TimeSpan? timeSpan = null)
+        {
+            Debug.WriteLine($"[MockDialogService]: {message}");
+        }
+
+        public void ShowToast(ToastConfig toastConfig)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MyProject/MyProject.UnitTests/ViewModels/MainViewModelTests.cs
@@ -0,0 +1,49 @@
+﻿using MyProject.UnitTests.Mocks.Persistence;
+using MyProject.UnitTests.Mocks.Services;
+using MyProject.ViewModels;
+using NUnit.Framework;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyProject.UnitTests.ViewModels
+{
+    [TestFixture]
+    public class MainViewModelTests
+    {
+        private MainViewModel ViewModel { get; set; }
+
+        [SetUp]
+        public async Task SetUp()
+        {
+            ViewModel = new MainViewModel(new MockDialogService(), new MockNavigationService(), new MockUnitOfWork());
+            await ViewModel.InitializeAsync(null);
+        }
+
+        [Test]
+        public void InitializeAsync_WhenCalled_ShouldInitializeProductsCollection()
+        {
+            // Test InitializeAsync method called in the set up method
+            Assert.That(ViewModel.Products, Is.Not.Null);
+            Assert.That(ViewModel.Products.Count, Is.EqualTo(5));
+        }
+
+        [Test]
+        public void AddCommand_WhenCalled_AddProductToProductsCollection()
+        {
+            ViewModel.AddCommand.Execute(null);
+
+            Assert.That(ViewModel.Products.Count, Is.EqualTo(6));
+            Assert.That(ViewModel.Products.Last().Id, Is.EqualTo(6));
+        }
+
+        [Test]
+        public void RemoveProductCommand_WhenCalled_RemoveProductFromProductsCollection()
+        {
+            var product = ViewModel.Products.First();
+            ViewModel.RemoveProductCommand.Execute(product);
+
+            Assert.That(ViewModel.Products.Count, Is.EqualTo(4));
+            Assert.That(ViewModel.Products.Contains(product), Is.False);
+        }
+    }
+}
