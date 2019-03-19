93b729dd91d56217c4ee906cbb9d8f432923dc23
danielccunha <danielcunha54@gmail.com>
2019-03-19 14:01:41 -0300

    Add tests for IAsyncCommand

---
+++ MyProject/MyProject.UnitTests/ViewModels/Commands/AsyncCommandTests.cs
@@ -0,0 +1,42 @@
+﻿using MyProject.UnitTests.Base;
+using MyProject.ViewModels.Base.Commands;
+using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+
+namespace MyProject.UnitTests.ViewModels.Commands
+{
+    public class AsyncCommandTests : BaseTestFixture
+    {
+        [Test]
+        public void Constructor_ExecuteIsNull_ThrowArgumentNullException()
+        {
+            Func<Task> execute = null;
+            Assert.That(() => new AsyncCommand(execute), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Constructor_ExecuteWithParameterIsNull_ThrowArgumentNullException()
+        {
+            Func<object, Task> execute = null;
+            Assert.That(() => new AsyncCommand(execute), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Constructor_CanExecuteIsNull_ThrowArgumentNullException()
+        {
+            Func<bool> canExecute = null;
+            Assert.That(() => new AsyncCommand(ExecuteMethod, canExecute), Throws.ArgumentNullException);
+        }
+
+        [Test]
+        public void Constructor_CanExecuteWithParameterIsNull_ThrowArgumentNullException()
+        {
+            Func<object, bool> canExecute = null;
+            Assert.That(() => new AsyncCommand(ExecuteMethodWithParameter, canExecute), Throws.ArgumentNullException);
+        }
+
+        private static Task ExecuteMethod() => Task.FromResult(true);
+        private static Task ExecuteMethodWithParameter(object obj) => Task.FromResult<object>(true);
+    }
+}
---
+++ MyProject/MyProject/MyProject/Models/MenuItem.cs
@@ -0,0 +1,9 @@
+﻿namespace MyProject.Models
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+        public string Page { get; set; }
+    }
+}
